8081cc8c8f1fbe7d4cc57f4f890d9afb490e019d
Ruben Moreel <ruben.moreel@vliz.be>
2023-09-25 15:47:30 +0200

    new dark mode colors and working

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,17 +4,24 @@ window.addEventListener('DOMContentLoaded', init)
 function init(){
     const slider = document.getElementById("slider");
     const body = document.body;
+    const modes = document.querySelectorAll('.game-mode');
+    const selector = document.querySelector('.game-selector')
+    const darkSelector = document.querySelector('.dark-game-selector')
 
     slider.addEventListener("input", function () {
         const sliderValue = slider.value;
         if (sliderValue === "0") {
             body.classList.remove("dark-mode");
+            selector.style.display= "block";
+            darkSelector.style.display = "none";
         } else {
             body.classList.add("dark-mode");
+            selector.style.display= "none";
+            darkSelector.style.display = "block";
         }
     });
 
-    const modes = document.querySelectorAll('.game-mode');
+    
 
     modes.forEach(el => el.addEventListener("click", function(event){
         const parentId = el.id;

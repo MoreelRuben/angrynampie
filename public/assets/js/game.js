f5772702e5e5b083c2aa3e96c86b73ed4fa2f9da
Ruben Moreel <ruben.moreel@vliz.be>
2023-09-25 14:26:34 +0200

    more js

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -2,6 +2,7 @@ window.addEventListener('DOMContentLoaded', init)
 
 
 function init(){
+    let counter = 25;
     let tiles = document.querySelectorAll('.game-tile');
 
     tiles.forEach(el => el.addEventListener("click", clickTile))
@@ -13,4 +14,22 @@ function clickTile(e){
     let tile = document.querySelector('#' + id);
     tile.removeEventListener;
     tile.classList.add("used")
+    const randomNumber = Math.floor(Math.random() * (counter - 1 + 1)) + 1;
+    if(randomNumber == counter){
+        endGame()
+    }
+    counter--
+}
+
+function endGame(){
+    window.addEventListener("click", toGameSelector);
+    document.querySelector("#endscreen").computedStyleMap.display  = "block";
+
+}
+
+
+function toGameSelector(e){
+    e.preventDefault;
+    window.location.href = "index.html";
+
 }
